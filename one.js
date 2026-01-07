ca55160ce988a0445f99da41ef60a28f4ae2acb9
Adrishya-0 <hari-plaza2@gmail.com>
2026-01-07 18:31:46 +0545

    07 changes plaza button added

---
+++ one.js
@@ -0,0 +1,29 @@
+ arr=[1,2,"shaurav","sajan"]
+ console.log(arr)
+
+ arr.push("Saraj")
+
+console.log(arr)
+
+
+one = [
+    {
+        name:"balendra shah",
+        age: 38,
+        
+    },
+
+    {
+        name:" harka sampang",
+        age: 44,
+    }
+]
+
+
+
+
+console.log(one)
+
+one.map((find)=>{
+    console.log(find)
+})
